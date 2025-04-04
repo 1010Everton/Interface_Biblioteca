e8025a3aee399088c49462f0d104f2d2d59f615b
EvErToN <evertonflorenciodasilva@outlook.com>
2025-04-04 09:27:17 -0300

    criei a interface com a escrita do login e a senha e o botão

--- src/main/java/com/example/interface_bliblioteca/HelloApplication.java
+++ src/main/java/com/example/interface_bliblioteca/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.interface_bliblioteca;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +13,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Label Login = new Label("Login");
+        Label Senha = new Label("senha");
 
+        Button Botao_confirma = new Button();
+
+
+        VBox box = new VBox();
+        box.getChildren().add(Login);
+        box.getChildren().add(Senha);
+        box.getChildren().add(Botao_confirma);
+
+        Scene cena = new Scene(box,400,400);
+        stage.setScene(cena);
+        stage.show();
     }
 
     public static void main(String[] args) {
