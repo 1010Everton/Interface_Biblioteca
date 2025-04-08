b521876ae071ea79f79cd4fec9fd9a11ed877802
EvErToN <evertonflorenciodasilva@outlook.com>
2025-04-08 07:56:01 -0300

    campo de login funcionando

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module Interface.Bliblioteca {
+    exports com.example.interface_bliblioteca to javafx.graphics;
     requires javafx.graphics;
+    requires javafx.fxml;
     requires javafx.controls;
     requires jakarta.persistence;
     requires com.google.gson;
-    requires javafx.fxml;
     requires org.hibernate.orm.core;
-
     opens com.example.interface_bliblioteca.Bibliotecas.Livros to org.hibernate.orm.core;
     opens com.example.interface_bliblioteca.Bibliotecas.Usuarios to org.hibernate.orm.core;
     opens com.example.interface_bliblioteca.Bibliotecas.Usuarios.adm to org.hibernate.orm.core;
